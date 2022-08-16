87b2fdb6508006f3b2eec4535682282e676db3fe
MaazTariq409 <maaztariq409@gmail.com>
2022-08-16 11:02:34 +0500

    admin panel

--- Cleanasia - FYP/Controllers/AdminController.cs
+++ Cleanasia - FYP/Controllers/AdminController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cleanasia.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCuisine.ViewModels;
+using System.Threading.Tasks;
 
 namespace Cleanasia.Controllers
 {
 	public class AdminController : Controller
 	{
-		public IActionResult Index()
+		private readonly CleanasiaContext _context;
+
+		public AdminController(CleanasiaContext context)
 		{
-			return View();
+			_context = context;
+		}
+
+		public async Task<IActionResult> Administration()
+		{
+			ProductViewModel productViewModel = new ProductViewModel();
+
+			productViewModel.categories = await _context.category.ToListAsync();
+			productViewModel.services = await _context.Service.ToListAsync();
+
+			return View(productViewModel);
 		}
 	}
 }
---
+++ Cleanasia - FYP/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Cleanasia.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCuisine.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<CategoryModel> categories { get; set; }
+        public List<ServiceModel> services { get; set; }
+    }
+}
